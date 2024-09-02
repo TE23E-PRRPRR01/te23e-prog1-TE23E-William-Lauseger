e1b40cb41206587d3818953daf227a23df78b29f
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-09-02 14:53:08 +0200

    Tränat på If satser

---
+++ Kap-3/EngChart/Program.cs
@@ -0,0 +1,55 @@
+﻿// Program som simulerar kända Engineering Flowchart
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörs flödeschema");
+
+// Visa en ASCii art
+Console.WriteLine($"""
+ $"   *.      
+              d$$$$$$$P"                  $    J
+                  ^$.                     4r  "
+                  d"b                    .db
+                 P   $                  e" $
+        ..ec.. ."     *.              zP   $.zec..
+    .^        3*b.     *.           .P" .@"4F      "4
+  ."         d"  ^b.    *c        .$"  d"   $         %
+ /          P      $.    "c      d"   @     3r         3
+4        .eE........$r===e$$$$eeP    J       *..        b
+$       $$$$$       $   4$$$$$$$     F       d$$$.      4
+$       $$$$$       $   4$$$$$$$     L       *$$$"      4
+4         "      ""3P ===$$$$$$"     3                  P
+ *                 $       "'"        b                J
+  ".             .P                    %.             @
+    %.         z*"                      ^%.        .r"
+       "*==*""                             ^"*==*""   
+""");
+
+Console.Write("Does it move? (y/n) ");
+string answer = Console.ReadLine();
+
+// Kolla om svaret är ja eller nej
+if (answer == "y")
+{ // y
+    Console.Write("Should it? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("no problem! ");
+    }
+    else
+    {
+        Console.WriteLine("plz use ducktape! ");
+    }
+}
+else
+{ // n
+    Console.Write("Should it? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "n")
+    {
+        Console.WriteLine("no problem");
+    }
+    else
+    {
+        Console.WriteLine("plz use WD-40! ");
+    }
+}
---
+++ Kap-3/InteractivStory/Program.cs
@@ -0,0 +1,16 @@
+﻿// Att göra en interaktiv berättelse
+
+Console.Clear();
+Console.WriteLine("Du kommer fram till 2 dörrar. Vilken dörr väljer du? (1/2) ");
+string answer = Console.ReadLine();
+if (answer == "1")
+{
+    Console.WriteLine("Du blev dödad av en haj");
+    if (answer == "2")
+    {
+        Console.WriteLine("Du vann 1 miljon dollar av Mr Beast");
+    }
+}
+
+
+
