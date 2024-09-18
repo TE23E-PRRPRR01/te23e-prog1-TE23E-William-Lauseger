43ef96d9d748f1df3f0f5964adabf56ebe0d241c
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-09-18 15:06:48 +0200

    Uppgift loop-quiz

---
+++ Kap-4/Loop-Quiz/Program.cs
@@ -0,0 +1,69 @@
+﻿// Quiz om NFL
+using System.Formats.Asn1;
+
+Console.Clear();
+Console.WriteLine("Välkommen till Quiz om NFL");
+
+Console.WriteLine("Vad är ditt namn?");
+string namn = Console.ReadLine()!;
+
+while (true)
+{
+    Console.Write("Fråga 1: Vilket lag vann Super Bowl senast? (Chiefs/Eagles/49ers)");
+    string lag = Console.ReadLine()!.ToLower();
+
+    if (lag == "chiefs")
+    {
+        Console.WriteLine("Korrekt");
+    }
+    else
+    {
+        Console.WriteLine("Inkorrekt");
+    }
+
+
+    Console.WriteLine("Fråga 2: Hur många gånger vann Patriots Super Bowl? (4/9/6)");
+    string gånger = Console.ReadLine()!;
+
+    if (gånger == "6")
+    {
+        Console.WriteLine("Korrekt");
+    }
+    else
+    {
+        Console.WriteLine("Inkorrekt");
+    }
+
+    Console.WriteLine("Fråga 3: Vilket efternamn har den bästa Quarterbacken genom alla tider? (Brady/Montana/Mahomes)");
+    string efternamn = Console.ReadLine()!.ToLower();
+
+    if (efternamn== "brady")
+    {
+        Console.WriteLine("Korrekt");
+    }
+    else
+    {
+        Console.WriteLine("Inkorrekt");
+    }
+    
+    Console.WriteLine("Fråga 4: Hur många poäng ger en Touchdown? (7/6/9)");
+    string poäng = Console.ReadLine()!;
+
+    if (poäng== "6")
+    {
+        Console.WriteLine("Korrekt");
+    }
+    else
+    {
+        Console.WriteLine("Inkorrekt");
+    }
+
+    Console.WriteLine($"{namn}, vill du köra igen? (y/n)");
+    string answer = Console.ReadLine()!;
+     if (answer== "n")
+    {
+        break;
+    }
+}
+
+Console.WriteLine("Tack för att du har spelat mitt Quiz om NFL");
