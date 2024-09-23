6fd8ef7af05a00e9ed0870fcc989caaf86b420e2
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-09-23 14:51:25 +0200

    Tränat på while loop och text hantering

---
+++ Kap-4/MenyProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿// Litet program med meny
+Console.Clear();
+Console.WriteLine("litet program med meny");
+
+// variabel för valet
+string val = "";
+
+// Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    1. omvandla en text till versaler
+    2. omvandla en text till gemener
+    3. avsluta
+    Välj ett av alternativen ovan:
+    """);
+    val = Console.ReadLine()!;
+
+    // Hantera användarens val
+    if (val== "1")
+    {
+        Console.Write("Skriv in en text");
+        string texten = Console.ReadLine()!.ToUpper();
+        Console.WriteLine($"Texten i versaler blir:{texten}");
+    }
+    else if (val== "2")
+    {
+        Console.Write("Skriv in en text");
+        string texten = Console.ReadLine()!.ToLower();
+        Console.WriteLine($"Texten i versaler blir:{texten}");
+    }
+    else if (val== "3")
+    {
+       Console.WriteLine("avslutar...");
+       break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde inget giltigt alternativ, Försök igen");
+    }
+}
---
+++ Kap-4/Texteditor/Program.cs
@@ -0,0 +1,50 @@
+﻿// Program som läser från fil och skriver till fil
+using System.Runtime.CompilerServices;
+
+Console.Clear();
+Console.WriteLine("Program som läser från fil och skriver till fil");
+
+// Variabel för valet
+string val = "";
+
+// Programloop som håller på tills användaren vill avsluta
+while (true)
+{
+    // Visar meny och läser användarens val
+    Console.Write("""
+    1. Skriv till fil
+    2. Läs till fil
+    3. Avsluta
+    Välj ett av alternativen ovan :
+    """);
+    val = Console.ReadLine()!;
+
+    // Hantera användarens val
+    if (val== "1")
+    {
+        // Skriv in text till fil
+        Console.Write("Skriv text till filen");
+        string texten = Console.ReadLine()!;
+        File.WriteAllText("filenamn.txt", texten);
+    }
+    else if (val== "2")
+    {
+        if (File.Exists("filnamn.txt"))
+        {
+            string texten = File.ReadAllText("filnamn.txt");
+        }
+        else
+        {
+            Console.WriteLine("Filen finns inte!");
+        }
+    }
+    else if (val== "3")
+    {
+        Console.WriteLine("avslutar...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde inget giltigt alternativ, Försök igen");
+    }
+}
