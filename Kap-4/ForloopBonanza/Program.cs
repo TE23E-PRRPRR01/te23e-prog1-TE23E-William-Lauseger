8881bbb75dda2a0af14673d95cb942182dab9082
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-11-20 15:01:45 +0100

    Tränat for-loop

---
+++ Kap-4/ForloopBonanza/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+// Console.Write("Ange startvärde: ");
+// int startvärde = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Ange slutvärde: ");
+// int slutvärde = int.Parse(Console.ReadLine()!);
+// int summa = 0;
+// for (int i = startvärde; i < slutvärde + 1; i++)
+// {
+//     Console.WriteLine(i);
+//     summa += i;
+// }
+// Console.WriteLine($" Summan av alla heltal är {summa}");
+
+// Console.Write("Ange startvärde2: ");
+// int startvärde2 = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Ange slutvärde2: ");
+// int slutvärde2 = int.Parse(Console.ReadLine()!);
+Console.Write("Jämna siffror: ");
+for (int i = 0; i < 21; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
+Console.WriteLine();
+
+Console.Write("Udda siffror: ");
+for (int i = 0; i < 21; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.Write($"{i}, ");
+    }
+}
