21d547839dee2a49f715fec25b6c5c728d751818
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-11-25 14:50:38 +0100

    Har tränat på list, string.join och add

---
+++ Kap-5/MikroOvningar1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Samlingar");
+
+// Skapa en lista med namn
+List<string> listaNamn = ["William", "Oliver", "Alma"];
+
+// Skriv ut listan med namn
+string join = string.Join(", ", listaNamn);
+Console.WriteLine(join);
+
+// Låt användaren skriva in ett namn som ska läggas till
+Console.Write("Ange ett namn att lägga in: ");
+string nyttNamn = Console.ReadLine()!;
+
+// Skriv in namnet i listan
+listaNamn.Add(nyttNamn);
+Console.WriteLine($"Alla namn efter tillägget: {string.Join(", ", listaNamn)}");
+
+// Ange ett nytt namn att lägga till
+Console.Write("Ange ett nytt namn att lägga till: ");
+string nyttNamn2 = Console.ReadLine()!;
+
+// Skriv ut det nya namnet i listan
+listaNamn.Add(nyttNamn2);
+Console.WriteLine($"Alla namn efter tillägget: {string.Join(", ", listaNamn)}");
+
