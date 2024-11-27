b477100d31671461d0140e25787634488aaaac43
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-11-27 15:04:10 +0100

    Tränat på listor och input och output

---
+++ Kap-5/MikroOvningar2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+// Skapa en lista med 5 namn
+List<string> listaNamn = ["Uzayon", "Oliver", "Alma", "Sandra", "Olga"];
+Console.WriteLine($"Mina kompisar är: {string.Join(", ", listaNamn)}");
+
+// Skapa en lista med 5 städer
+List<string> listaStäder = ["Detroit", "Miami", "Dallas", "New York", "Los Angeles"];
+Console.WriteLine($"5 städer {string.Join(", ", listaStäder)} som ligger i USA");
+
+// Skapa en lista med 5 årtal
+List<int> listaÅrtal = [1891, 1945, 2007, 2015, 2024];
+Console.WriteLine($"5 viktiga årtal: '{string.Join("', '", listaÅrtal)}'");
+
+// Lägg till och ta bort namn i listan
+List<string> listaDryck = ["Coca Cola", "fanta", "Pomac", "7Up", "Bloody Mary"];
+Console.WriteLine($"Nuvarande lista: {string.Join(", ", listaDryck)}");
+Console.Write("Ange dryck att lägga till: ");
+List<string> nyDryck = ["Coca cola", "fanta", "pomac", "7up", "bloody mary"];
