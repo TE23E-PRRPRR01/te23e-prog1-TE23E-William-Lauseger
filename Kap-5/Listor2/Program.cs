94198e91e7232e0013551b9f1474c0922263acf7
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2024-12-16 14:51:02 +0100

    Tränat på listor inför prov

---
+++ Kap-5/Listor2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.WriteLine("Allt om listor");
+
+// Skapa en tom lista
+List<string> listaNamn = [];
+
+// Infoga i en lista
+listaNamn.Add("William");
+listaNamn.Add("Oliver");
+listaNamn.Add("Alma");
+Console.WriteLine(listaNamn.Count);
+
+// Skriv ut listan
+Console.WriteLine($"Lista: {listaNamn}");
+
+// Skriv ut listan med hjälp av en loop
+foreach (var namn in listaNamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+// Skriv ut första elementet i listan (index = 0)
+Console.WriteLine($"Första elementet: {listaNamn[0]}");
+
+// Skriv ut två namn ut listan
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"{i}: {listaNamn[1]}");
+}
+
+// Skriv ut alla namn i listan med for-loop
+for (int i = 0; i < listaNamn.Count; i++)
+{
+    
+}
---
+++ Kap-5/MikroOvningar4/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Film register");
+
+// Skapa en lista med actionspel
+List<string> listaAction = [];
+
+// Skapa en lista med äventyrsspel
+List<string> listaAdventure = [];
+
+// Skapa en fråga för att lägga till fler spel
+Console.WriteLine("Nu ska du lägga till 2 actionspel i listan: ");
+Console.Write("Ange 1 actionspel att lägga till: ");
+string spel = Console.ReadLine()!;
+listaAction.Add(spel);
+Console.Write("Ange 1 actionspel att lägga till: ");
+spel = Console.ReadLine()!;
+listaAction.Add(spel);
+
+// Fyll på listan med äventyrsspel med hjälp av for-loop
+Console.WriteLine("Du ska nu fylla i 3 äventyrsspel: ");
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Ange 1 äventyrsspel att lägga till: ");
+    spel = Console.ReadLine()!;
+    listaAction.Add(spel);
+}
+
+Console.WriteLine("Du ska nu fylla i 2 actionspel: ");
+for (int i = 0; i < 2; i++)
+{
+    Console.Write("Ange 1 actionspel att lägga till: ");
+    spel = Console.ReadLine()!;
+    listaAction.Add(spel);
+}
