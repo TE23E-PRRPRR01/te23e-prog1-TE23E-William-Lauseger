2f37d29bd6a87aaa04a37874e8d087aed27e12ae
William Lauseger <William.larssonlauseger@elev.ga.ntig.se>
2025-01-08 15:04:59 +0100

    Första introduktion till Metoder

---
+++ Kap-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,35 @@
+﻿/* ****************************
+* ******** M A I N ************
+******************************* */
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejNamn("William");
+SägHejNamn("Oliver");
+SägHejNamn("Jemila");
+SägHejNamn("");
+KvadreraTal();
+
+/* ****************************
+* ******* M E T O D E R *******
+******************************* */
+
+/// <summary>
+/// Skriv ut hälsning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
+
+
+/// <summary>
+/// Metod som skriver ett tal i kvadrat
+/// <summary>
+/// <param name="Tal"></param>
+static void KvadreraTal(int Tal)
+{
+    Console.WriteLine($"tal i kvadrat = {Tal * Tal}");
+}
---
+++ Kap-6/MetoderIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿/* ****************************
+* ********** M A I N **********
+******************************* */
+
+Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+// Anropa/Använd metoden
+SägHej();
+SägHej();
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+static void SägHej()
+{
+    Console.Write("Hej på dig!");
+}
+
+/// <summary>
+/// Metod för att visa en meny
+/// </summary>
+
+VisaMeny();
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut alla uppgifter
+    4. Avsluta
+    """);
+}
+
+
+
+/* ****************************
+* ******* M E T O D E R *******
+******************************* */
+
+// Anropa metoden RegistreraNamn()
+RegistreraNamn();
+
+/// <summary>
+/// Metod för att ange namn
+/// </summary>
+static void RegistreraNamn()
+{
+    Console.WriteLine("Ange namn: ");
+    string namn = Console.ReadLine()!;
+    Console.WriteLine(namn);
+}
